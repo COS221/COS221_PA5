8e0083ca7ce4f41a0b1554eaa860072adf874fd6
Ruan Carlinsky <ruancarlinsky01@gmail.com>
2023-05-30 09:59:09 +0200

    Modal Functionality Update

--- wineries.js
+++ wineries.js
@@ -1,31 +1,35 @@
+
+
+
+    
+
 function populateExploreWineries() {
   const xhr = new XMLHttpRequest();
-  const url = 'http://localhost/COS221_PA5/api.php';
+  const url = "http://localhost/COS221_PA5/api.php";
 
   var data = {
-    "type":"GetAllWineries",
-    "api_key":"123456",
-    "page": "index.php",
+    type: "GetAllWineries",
+    api_key: "123456",
+    page: "index.php",
     //"search":{"BName":"Franschhoek Cellar"},
-    "order":"desc",
+    order: "desc",
     //"limit":10,
-    "sort":"BName",
-    "fuzzy":false,
-    "return":"*"
-  }
+    sort: "BName",
+    fuzzy: false,
+    return: "*",
+  };
 
-  xhr.onreadystatechange = function(){
-      if (xhr.readyState == 4 && xhr.status == 200) {
-          let objectData = JSON.parse(this.responseText); 
-          
-          let row = document.querySelector('row');
-          row.innerHTML = '';
-          
-          data= ``;
-          
-          for (value in objectData){
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let objectData = JSON.parse(this.responseText);
 
-              data += `<div class="col-md">
+      let row = document.querySelector(".row");
+      //row.innerHTML = '';
+
+      data = ``;
+
+      for (i = 0; i < objectData.data.length; i++) {
+        data += `<div class="col-md">
               <div class="card" style="color: black">
                 <div class="card-body text-center">
                   <div class="h1 mb-3">
@@ -33,28 +37,56 @@ function populateExploreWineries() {
                           margin: 15px;
                           border-radius: 15px;
                           box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-                        " src="img/vineyard-placeholder.jpg" />
+                        " src="${objectData.data[i].Business_URL}" />
                   </div>
-                  <h3 class="card-title mb-3">${objectData[value].data[0].BName}</h3>
+                  <h3 class="card-title mb-3">${objectData.data[i].BName}</h3>
                   <p>Region</p>
                   <p class="card-text">
-                  ${objectData[value].data[0].Description}
+                  ${objectData.data[i].Description}
                   </p>
-                  <a href="wineriesSpec.php" class="btn btn-primary" style="background-color: #00192b">Read More</a>
+                  <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" style="background-color: #00192b;">Learn More</button>
                 </div>
               </div>
-            </div>`
-
-          }
-          row.innerHTML=data;
-
-
-      }else {
-          console.log("Api link not accessible.");
+            </div>`;
       }
-  }
 
-  xhr.open('POST', url, true)
-  xhr.setRequestHeader('Content-type','application/json');
-  xhr.send(JSON.stringify(data));  
+      data += `<div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+          <div class="modal-body" style="background-color: whitesmoke; border-radius: 10px; padding: 50px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);">
+            <div class="row">
+              <div class="col-3">
+                <img src="img/vineyard-placeholder.jpg" alt="hrw-default" id= "wineryImage" style="margin: 15px; border-radius: 15px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);" />
+              </div>
+              <div class="col-4" style="margin-left: 50px; margin-top: 10px">
+                <ul style="list-style-type: none">
+                  <li class="liTitle">
+                    <h1>Specific Winery</h1>
+                  </li>
+                  <li class="liAddress">42 Werner Street, Stellenbosch</li>
+                  <li class="liDescription">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis iure dolorem esse facilis in, perspiciatis repudiandae et ipsam? Enim pariatur dolore ipsum nostrum id. Repellendus, dolor. Optio laborum minus dolorem!
+                  </li>
+                </ul>
+              </div>
+              <div class="col-4">
+                <div id="map"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+      row.innerHTML = data;
+    } else {
+      console.log("Api link not accessible.");
+    }
+  };
+
+  
+
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.send(JSON.stringify(data));
+
 }
--- wines.js
+++ wines.js
@@ -16,38 +16,143 @@ function populateExploreWines() {
 
   xhr.onreadystatechange = function(){
     if (xhr.readyState == 4 && xhr.status == 200) {
-        let objectData = JSON.parse(this.responseText); 
-
-          let row = document.querySelector('row');
-          row.innerHTML = '';
+        
+        let objectData = JSON.parse(this.responseText);
+          
+        var row = document.querySelector('.row');
+          
           data= ``;
           
-          for (value in objectData){
-
+          for (i=0; i < objectData.data.length; i++){
+              
               data += `<div class="col-md">
               <div class="card" style="color: black">
                 <div class="card-body text-center">
                   <h3 class="card-title" style="margin-top: 10px; margin-bottom: 0px">
-                    ${objectData[value].data[0].Name}
+                    ${objectData.data[i].Name}
                   </h3>
-                  <h8 class="card-cultivar">category | ${objectData[value].data[0].Category}</h8>
+                  <h8 class="card-cultivar">category | ${objectData.data[i].Category}</h8>
                   <h4 class="card-rating mb-3" style="margin-top: 0; padding-top: 0px">
                     ★★★★☆
                   </h4>
                   <div class="card-img-container" style="border-radius: 5px">
-                    <img src="${objectData[value].data[0].Wine_URL}" />
+                    <img src="${objectData.data[i].Wine_URL}" />
                   </div>
-                  <h3 class="card-price" style="margin: 0px; margin-bottom: 15px">
-                  ${objectData[value].data[0].Name}
+                  <h3 class="card-price" style="margin: 0px; margin-bottom: 15px"> R
+                  ${objectData.data[i].Cost_per_bottle}
                   </h3>
-                  <a href="wineSpec.php" class="btn btn-primary" style="background-color: #00192b; border: none">Learn
-                    More</a>
+                  <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" style="background-color: #00192b;">Learn More</button>
                 </div>
               </div>
             </div>`
           }
-          row.innerHTML=data;
 
+          data+= `<div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+              <!-- Move the 'container' content into the modal-body -->
+              <div class="modal-body" style="background-color: whitesmoke; border-radius: 10px; padding: 50px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);">
+                <div class="row">
+                  <div class="col-3">
+                    <img src="img/hrw-default-300x300.jpg" alt="hrw-default" class="img-fluid" style="margin: 15px; border-radius: 15px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);" />
+                    <h1>Specific Wine</h1>
+                  </div>
+                  
+                  <div class="col-4" style="margin-left: 50px; margin-top: 10px">
+                    <ul style="list-style-type: none">
+                      <li class="liRating">
+                        <h3>★★★★☆</h3>
+                      </li>
+                      <li class="liCategoryCultivar">Category & Cultivar</li>
+                      <li class="liAlc">Alc: 12% pH3.43 TA: 5.32</li>
+                      <li class="liProd">Producer</li>
+                      <li>
+                        <h2 style="margin-top: 30px">R1500.00</h2>
+                      </li>
+                      <li>
+                        <a href="#" class="btn btn-primary" style="background-color: #00192b">Buy Now</a>
+                      </li>
+                    </ul>
+                  </div>
+        
+                  <div class="col-4">
+                    <ul class="scrollReviews nav-pills nav-stacked" style="list-style-type: none">
+                      <li>
+                        <h1>Reviews</h1>
+                      </li>
+                      <li>
+                        <div class="review">
+                          <ul style="list-style-type: none">
+                            <li>
+                              <h4>Username</h4>
+                            </li>
+                            <li>
+                              <h5>★★★☆☆</h5>
+                            </li>
+                            <li>
+                              <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                Facilis aperiam consequatur natus recusandae dicta numquam
+                                inventore perferendis magnam deserunt neque doloremque
+                                dignissimos aliquid, vitae doloribus earum architecto
+                                illum. Sequi, reiciendis?
+                              </p>
+                            </li>
+                          </ul>
+                        </div>
+                      </li>
+        
+                      <li>
+                        <div class="review">
+                          <ul style="list-style-type: none">
+                            <li>
+                              <h4>Username</h4>
+                            </li>
+                            <li>
+                              <h5>★★★☆☆</h5>
+                            </li>
+                            <li>
+                              <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                Facilis aperiam consequatur natus recusandae dicta numquam
+                                inventore perferendis magnam deserunt neque doloremque
+                                dignissimos aliquid, vitae doloribus earum architecto
+                                illum. Sequi, reiciendis?
+                              </p>
+                            </li>
+                          </ul>
+                        </div>
+                      </li>
+        
+                      <li>
+                        <div class="review">
+                          <ul style="list-style-type: none">
+                            <li>
+                              <h4>Username</h4>
+                            </li>
+                            <li>
+                              <h5>★★★☆☆</h5>
+                            </li>
+                            <li>
+                              <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                Facilis aperiam consequatur natus recusandae dicta numquam
+                                inventore perferendis magnam deserunt neque doloremque
+                                dignissimos aliquid, vitae doloribus earum architecto
+                                illum. Sequi, reiciendis?
+                              </p>
+                            </li>
+                          </ul>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`
+          row.innerHTML = data;
 
       }else {
           console.log("Api link not accessible.");
@@ -58,3 +163,4 @@ function populateExploreWines() {
   xhr.setRequestHeader('Content-type','application/json');
   xhr.send(JSON.stringify(data));  
 }
+
