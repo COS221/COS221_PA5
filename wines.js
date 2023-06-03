d04b67181cc3604a1d76c720a51a9ccb1f69a8d1
aliyahlimbada <u22621522@tuks.co.za>
2023-06-03 11:13:17 +0200

    filtering added to wines.js

--- wines.js
+++ wines.js
@@ -219,46 +219,32 @@ document.addEventListener("click", function (event) {
   }
 });
 
-function applyFilter() {
-  var dropdown = document.getElementById("navbarDropdownMenuLink");
-  var selectedOption = dropdown.textContent.trim();
-  
-  executeFilter(selectedOption);
-}
 
-var applyButton = document.getElementById("applyFilterButton");
-applyButton.addEventListener("click", applyFilter);
+//sort function
+function sortData(text) {
 
-function executeFilter(option) {
-  console.log(option);
-  var selected_option = option;
-
-  var getactualOption = selected_option.split(':');
-  console.log(getactualOption);
-  var realOption = getactualOption[1].trim();
+  var realOption = text;
 
   var parts = realOption.split('-');
-  var sort = parts[0].trim();
-  var order = parts[1].trim();
-
-  console.log(sort);
-  console.log("here is the order");
-  console.log(order);
-
+  var actual_sort = parts[0].trim();
+  var actual_order = parts[1].trim();
+  
   const xhr = new XMLHttpRequest();
   const url = "http://localhost/COS221_PA5/api.php";
 
   var data = {
     type: "GetAllWines",
     api_key: "123456",
-    page: "bruh",
-    //"search":{"wine_name":"Franschhoek Cellar"},
-    order: order,
+    //"page": "bruh",
+    //search: { Sweetness: text },
+    order: actual_order,
     //"limit":10,
-    sort: sort,
+    sort: actual_sort,
     fuzzy: false,
     return: "*",
   };
+
+  console.log(data);
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -269,32 +255,35 @@ function executeFilter(option) {
       data = ``;
 
       for (i = 0; i < objectData.data.length; i++) {
-        //alert(objectData.data[i].WineID);
-        var WineName = objectData.data[i].Name;
-        var underscoredName = WineName.replace(/\s/g, "_"); //Wine Name spaces replaced with underscore.
-        data += `<div class="col-md" >
-              <div class="card" style="color: black">
-                <div class="card-body text-center">
-                  <h5 class="card-title" style="margin-top: 10px; margin-bottom: 0px">
-                    ${objectData.data[i].Name}
-                  </h5>
-                  <h8 class="card-cultivar">category | ${objectData.data[i].Category}</h8>
-                  <h4 class="card-rating mb-3" style="margin-top: 0; padding-top: 0px">
-                    ★★★★☆
-                  </h4>
-                  <div class="card-img-container" style="border-radius: 5px;">
-                    <img src="${objectData.data[i].Wine_URL}" style="width: 230px; height: 210px;"/>
+            
+          var WineName = objectData.data[i].Name;
+          console.log(WineName);
+          var underscoredName = WineName.replace(/\s/g, "_"); //Wine Name spaces replaced with underscore.
+          console.log(underscoredName);
+          data += `<div class="col-md" >
+                <div class="card" style="color: black">
+                  <div class="card-body text-center">
+                    <h5 class="card-title" style="margin-top: 10px; margin-bottom: 0px">
+                      ${objectData.data[i].Name}
+                    </h5>
+                    <h8 class="card-cultivar">category | ${objectData.data[i].Category}</h8>
+                    <h4 class="card-rating mb-3" style="margin-top: 0; padding-top: 0px">
+                      ★★★★☆
+                    </h4>
+                    <div class="card-img-container" style="border-radius: 5px;">
+                      <img src="${objectData.data[i].Wine_URL}" style="width: 230px; height: 210px;"/>
+                    </div>
+                    <h3 class="card-price" style="margin: 0px; margin-bottom: 15px"> R
+                    ${objectData.data[i].Cost_per_bottle}
+                    </h3>
+                    <button class="ourSpecButton btn btn-primary " id="${underscoredName}" data-bs-toggle="modal" data-bs-target="#myModal" style="background-color: #00192b;">Learn More</button>
                   </div>
-                  <h3 class="card-price" style="margin: 0px; margin-bottom: 15px"> R
-                  ${objectData.data[i].Cost_per_bottle}
-                  </h3>
-                  <button class="ourSpecButton btn btn-primary " id="${underscoredName}" data-bs-toggle="modal" data-bs-target="#myModal" style="background-color: #00192b;">Learn More</button>
                 </div>
-              </div>
-            </div>`;
-      }
+              </div>`;
+    }
 
       row.innerHTML = data;
+
     } else {
       console.log("Api link not accessible.");
     }
@@ -303,8 +292,8 @@ function executeFilter(option) {
   xhr.open("POST", url, true);
   xhr.setRequestHeader("Content-type", "application/json");
   xhr.send(JSON.stringify(data));
-
 }
+
 
 // Search function
 //const search = (event) => {
@@ -380,7 +369,115 @@ function executeFilter(option) {
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify(data));
   }
+}
+
+  function filterData(newText) {
+
+    var selected_option = newText;
+    var getactualOption = selected_option.split(':');
+   
+    var attr=getactualOption[0].trim();
+    var val =getactualOption[1].trim();
+  
+    const xhr = new XMLHttpRequest();
+    const url = "http://localhost/COS221_PA5/api.php";
+  
+    if(attr==="Category")
+    {
+      var data = {
+        type: "GetAllWines",
+        api_key: "123456",
+        //"page": "bruh",
+        search: { Category: val },
+        order: "desc",
+        //"limit":10,
+        sort: "Acidity",
+        fuzzy: false,
+        return: "*",
+      };
+    }
+    else if(attr==="Price Category")
+    {
+      var data = {
+        type: "GetAllWines",
+        api_key: "123456",
+        //"page": "bruh",
+        search: { Price_Category: val },
+        order: "desc",
+        //"limit":10,
+        sort: "Acidity",
+        fuzzy: false,
+        return: "*",
+      };
+    }
+    else
+    {
+      var data = {
+        type: "GetAllWines",
+        api_key: "123456",
+        //"page": "bruh",
+        search: { Sweetness: val },
+        order: "desc",
+        //"limit":10,
+        sort: "Acidity",
+        fuzzy: false,
+        return: "*",
+      };
+    }
+   
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        let objectData = JSON.parse(this.responseText);
+  
+        var row = document.querySelector(".row");
+  
+        data = ``;
+  
+        for (i = 0; i < objectData.data.length; i++) {
+              
+            var WineName = objectData.data[i].Name;
+            var underscoredName = WineName.replace(/\s/g, "_"); //Wine Name spaces replaced with underscore.
+            data += `<div class="col-md" >
+                  <div class="card" style="color: black">
+                    <div class="card-body text-center">
+                      <h5 class="card-title" style="margin-top: 10px; margin-bottom: 0px">
+                        ${objectData.data[i].Name}
+                      </h5>
+                      <h8 class="card-cultivar">category | ${objectData.data[i].Category}</h8>
+                      <h4 class="card-rating mb-3" style="margin-top: 0; padding-top: 0px">
+                        ★★★★☆
+                      </h4>
+                      <div class="card-img-container" style="border-radius: 5px;">
+                        <img src="${objectData.data[i].Wine_URL}" style="width: 230px; height: 210px;"/>
+                      </div>
+                      <h3 class="card-price" style="margin: 0px; margin-bottom: 15px"> R
+                      ${objectData.data[i].Cost_per_bottle}
+                      </h3>
+                      <button class="ourSpecButton btn btn-primary " id="${underscoredName}" data-bs-toggle="modal" data-bs-target="#myModal" style="background-color: #00192b;">Learn More</button>
+                    </div>
+                  </div>
+                </div>`;
+      }
+  
+        row.innerHTML = data;
+
+      } else {
+        console.log("Api link not accessible.");
+      }
+    };
+  
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify(data));
   }
+  
+  
+  
+  
+  
+  
+
+  
   
   
   
